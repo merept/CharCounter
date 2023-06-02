964bec9febafb10b574f96cb5df496f6ef3ef5a5
merept <1466005267@qq.com>
2023-06-02 16:00:49 +0800

    colorful bat

--- install_modules.bat
+++ install_modules.bat
@@ -1,12 +1,34 @@
 @echo off
-echo install pandas
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
+:start
+call :ColorText 0b "install"
+call :ColorText 0a " pandas"
+echo=
 pip install pandas
 echo=
-echo install PyYAML
+call :ColorText 0b "install"
+call :ColorText 0a " PyYAML"
+echo=
 pip install PyYAML
 echo=
-echo install openpyxl
+call :ColorText 0b "install"
+call :ColorText 0a " openpyxl"
+echo=
 pip install openpyxl
 echo=
-echo Install finished
+call :ColorText 0a "Install finished"
+echo=
 pause
+
+goto :eof
+
+:ColorText
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1
+goto :eof
